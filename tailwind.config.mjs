91474b59065ed8be5a38fb5fa713274f45314ad2
krouu <159396847+krouu@users.noreply.github.com>
2025-02-05 12:50:42 +0900

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,8 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./widgets/**/*.{js,ts,jsx,tsx,mdx}",
+    "./share/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
